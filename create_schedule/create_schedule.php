b38244458fa5ccaf424c19f690f76ad3995eba3f
Amawers <dacoroon.christianjericho@gmail.com>
2024-01-17 12:55:43 +0800

    backend for creating schedules

---
+++ create_schedule/create_schedule.php
@@ -0,0 +1,63 @@
+<?php
+include('../connection.php'); 
+
+// orig
+// if(isset($_POST['teacher_id'], $_POST['subject_name'], $_POST['section_name'], $_POST['start_time'], $_POST['end_time'], $_POST['days_of_weeek'])){
+//     $teacherId  = $_POST['teacher_id'];
+//     $subjectName = $_POST['subject_name'];
+//     $sectionName = $_POST['section_name'];
+//     $startTime = $_POST['start_time'];
+//     $endTime = $_POST['end_time'];
+//     $daysOfWeek = $_POST['days_of_weeek'];
+// }else{
+//     $response = "wala na received from frontend";
+// }
+
+$teacherId  = '8';
+$subjectName = 'Mobile Programming';
+$sectionName = 'R7';
+$startTime = '08:00:00';
+$endTime = '12:00:00';
+$daysOfWeek = 'Monday';
+
+$query = "SELECT subjects.subject_id
+          FROM subjects
+          WHERE subject_name = '$subjectName'";
+$result = $connectNow->query($query);
+
+//use existing subject_id
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $subjectId = $row['subject_id'];
+} else {
+    echo 'error kuha subject_id';
+}
+
+$query = "SELECT sections.section_id
+          FROM sections
+          WHERE section_name = '$sectionName'";
+$result = $connectNow->query($query);
+
+//use existing section_id
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $sectionId = $row['section_id'];
+} else {
+    echo 'error kuha section_id';
+}
+
+$query = "INSERT INTO class_schedules (teacher_id, subject_id, section_id, start_time, end_time, day_of_week)
+VALUES ('$teacherId','$subjectId', '$sectionId', '$startTime', '$endTime', '$daysOfWeek')";
+    
+$result = $connectNow->query($query);
+
+if ($result) {
+    $response = "Data inserted successfully";
+} else {
+    $response = "Error inserting data: " . $connectNow->error;
+}
+
+echo json_encode($response);
+
+$connectNow->close();
+?>
---
+++ create_schedule/schedules_list.php
@@ -0,0 +1,37 @@
+<?php
+include('../connection.php'); 
+
+$teacherId = $_GET['teacher_id'];
+
+// //testing purpose
+// $teacherId  = '8';
+
+$query = "SELECT DISTINCT
+class_schedules.schedule_id,
+subjects.subject_name,
+sections.section_name,
+class_schedules.start_time,
+class_schedules.end_time,
+class_schedules.day_of_week
+FROM 
+class_schedules
+JOIN subject_teachers ON class_schedules.subject_id = subject_teachers.subject_id
+JOIN subjects ON class_schedules.subject_id = subjects.subject_id
+JOIN sections ON class_schedules.section_id = sections.section_id
+WHERE 
+subject_teachers.teacher_id = '$teacherId'";
+
+$result = $connectNow->query($query);
+
+if($result->num_rows > 0) {
+    $schedulesList = array();
+    while($rowFound = $result->fetch_assoc()) {
+        $schedulesList[] = $rowFound;
+    }
+    echo json_encode($schedulesList);
+}else{
+    echo "didn't receive";
+}
+$connectNow->close();
+
+?>
