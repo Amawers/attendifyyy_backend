96acd7ca56900f056646c2655474d0aafeb7a650
Amawers <dacoroon.christianjericho@gmail.com>
2024-01-21 16:39:49 +0800

    update_account backend

---
+++ account/update_account.php
@@ -0,0 +1,44 @@
+<?php
+include('../connection.php'); 
+$input_data = file_get_contents('php://input');
+
+parse_str($input_data, $decoded_data);
+
+if(isset($decoded_data['teacher_id'], $decoded_data['first_name'], $decoded_data['last_name'], $decoded_data['email'], $decoded_data['phone_number'], $decoded_data['department'])){
+    $teacherId = $decoded_data['teacher_id'];
+    $firstName = $decoded_data['first_name'];
+    $lastName = $decoded_data['last_name'];
+    $email = $decoded_data['email'];
+    $phoneNumber = $decoded_data['phone_number'];
+    $department = $decoded_data['department'];
+}else{
+    $response = "wala na received diri sa backend ang mga values";
+}
+
+// testing
+// $teacherId = '17';
+// $firstName = 'Jay Noel';
+// $lastName = 'Rojo';
+// $email = 'rojo@gmail.com';
+// $phoneNumber = '09797475869';
+// $department = 'IT';
+
+$query = "UPDATE teachers 
+SET first_name = '$firstName', 
+last_name = '$lastName',
+email = '$email',
+phone_number = '$phoneNumber',
+department = '$department' WHERE teacher_id = '$teacherId'";
+
+$result = $connectNow->query($query);
+
+
+if ($result) {
+    $response = "Data inserted successfully";
+} else {
+    $response = "Error inserting data: " . $connectNow->error;
+}
+echo json_encode($response);
+
+$connectNow->close();
+?>
